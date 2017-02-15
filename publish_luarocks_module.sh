a3ca5014858aa8a4ebf2dabdfa43d2aaea0084cb
Just Kh <last.khajiit@gmail.com>
2017-02-15 17:23:37 +0300

    Update publish_luarocks_module.sh

--- publish_luarocks_module.sh
+++ publish_luarocks_module.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-latesttag=$(git describe --tags)
-echo checking out ${latesttag}
-
 luaversion=${LUA}
 if [ "$luaversion" == "lua=5.2" ]; then
+	latesttag=$(git describe --tags)
+	echo "-------"
+	echo "Need to check for new releases (via tags)"
+	echo checking out ${latesttag}
+	echo "-------"
 	echo "$luaversion: we can publish module in this case!"
 else
 	echo "$luaversion: not valid case!"

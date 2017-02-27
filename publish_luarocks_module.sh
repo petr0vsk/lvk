67060dec90bb0f15f5cb3cc6872034cebd66a6c9
Just Kh <last.khajiit@gmail.com>
2017-02-27 15:12:18 +0300

    Update publish_luarocks_module.sh

--- publish_luarocks_module.sh
+++ publish_luarocks_module.sh
@@ -2,7 +2,7 @@
 
 current_artifact_version=$1
 lua_version=${LUA}
-latest_tag=$(git describe --tags)
+latest_tag=$(git describe --abbrev=0 --tags)
 
 #need to compare latest released version with latest published in Luarocks version too
 if [ "$lua_version" == "lua=5.2" ]; then

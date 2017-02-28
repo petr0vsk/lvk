4e0c2300f78be140190113df1c73fbb30e6ca964
Just Kh <last.khajiit@gmail.com>
2017-02-28 12:33:09 +0300

    fix build

--- publish_luarocks_module.sh
+++ publish_luarocks_module.sh
@@ -10,6 +10,6 @@ echo "Lua version = $lua_version"
 echo "Current lvk version = $current_artifact_version"
 
 #need to compare latest released version with latest published in Luarocks version too
-if [ "$lua_version" == "lua=5.2" ] && [ "$current_artifact_version" != "${latest_tag}" ] then
+if [ "$lua_version" == "lua=5.2" ] && [ "$current_artifact_version" != "${latest_tag}" ]; then
     echo "Need to publish new module"
 fi

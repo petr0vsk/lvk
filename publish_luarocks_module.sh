5b4cd05871ac708cd887cbc012bb41a4976373f8
Just Kh <last.khajiit@gmail.com>
2017-03-09 15:31:42 +0300

    Update publish_luarocks_module.sh

--- publish_luarocks_module.sh
+++ publish_luarocks_module.sh
@@ -4,10 +4,11 @@ current_artifact_version=$1
 lua_version=${LUA}
 latest_tag=$(git describe --abbrev=0 --tags)
 
-
-echo "Latest tag = ${latest_tag}"
-echo "Lua version = $lua_version"
-echo "Current lvk version = $current_artifact_version"
+echo "~~~~~~~~~~~~~~~~~"
+echo "~~ Latest tag = ${latest_tag}"
+echo "~~ Lua version = $lua_version"
+echo "~~ Current lvk version = $current_artifact_version"
+echo "~~~~~~~~~~~~~~~~~"
 
 #need to compare latest released version with latest published in Luarocks version too
 if [ "$lua_version" == "lua=5.2" ] && [ "$current_artifact_version" != "${latest_tag}" ]; then
